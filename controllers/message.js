e9648adb727a9ebe73776352084a5728e76be9cc
robert <runshacking@gmail.com>
2020-03-01 12:41:25 +0200

    rewrote the message controller

--- controllers/message.js
+++ controllers/message.js
@@ -5,27 +5,25 @@ class MessageController {
     this.socket = socket;
   }
 
-  readMessage = (res, userId, dialogId) => {
-    const filter = { dialog: dialogId, user: { $ne: userId } };
+  // readMessage = (res, userId, dialogId) => {
+  //   const filter = { dialog: dialogId, user: { $ne: userId } };
 
-    MessageModal.updateMany(filter, { $set: { readed: true } }, err => {
-      if (err) {
-        return res.status(500).json({ message: err });
-      }
-    });
-  }
+  //   MessageModal.updateMany(filter, { $set: { readed: true } }, err => {
+  //     if (err) {
+  //       return res.status(500).json({ message: err });
+  //     }
+  //   });
+  // }
 
   getMessages = async(req, res) => {
     const dialogId = req.query.dialog;
-    const userId = req.user._id;
 
-    this.readMessage(req, userId, dialogId);
     MessageModal
     .find({ dialog: dialogId })
     .populate("user")
     .exec((err, message) => {
       if (err) {
-        return  res.json({message: "Dialogs not found"});
+        return  res.json({message: "Messages not found"});
       }
 
       res.json(message);
@@ -98,47 +96,52 @@ class MessageController {
     });
   }
 
-  deleteMessage = (req, res) => {
-    const id = req.query.id;
-    const userId = req.user._id;
-
-    MessageModal.findById(id, async(err, message) => {
-      if (message.user.toString() === userId) {
-        const dialogId = message.dialog;
-        const query = { dialog: dialogId };
-
-        await message.remove();
-
-        MessageModal.findOne(query, {}, {sort: { createdAt: -1 }}, (err, lastMessage) => {
-          if (err) {
-            return res.status(500).json({
-              message: err,
-            });
-          }
-
-          DialogModal.findById(dialogId, (err, dialog) => {
-            if (err) {
-              return res.status(500).json({
-                message: err,
-              });
-            }
-
-            dialog.lastMessage = lastMessage;
-            dialog.save();
-          });
-
-          this.socket.emit('DIALOG_RECEIVED', {lastMessage});
-
-          MessageModal
-          .find({ dialog: dialogId })
-          .populate("user")
-          .exec((err, messages) => {
-            this.socket.emit('MESSAGE_RECEIVED', {messages});
-          });
+  updateMessages = dialogId => {
+    MessageModal
+      .find({ dialog: dialogId })
+      .populate("user")
+      .exec((err, messages) => {
+        this.socket.to('guys').emit('MESSAGE_RECEIVED', {
+          messages
         });
-      } else {
-        res.status(403).json({message: 'Not have permission'});
+      });
+  }
+
+  updateLastMessage = dialogId => {
+    MessageModal.findOne({ dialog: dialogId }, {}, {sort: { createdAt: -1 }}, (err, lastMessage) => {
+      if (err) {
+        return res.status(500).json({
+          message: err,
+        });
       }
+
+      DialogModal.findById(dialogId, (err, dialog) => {
+        if (err) {
+          return res.status(500).json({
+            message: err,
+          });
+        }
+
+        dialog.lastMessage = lastMessage;
+        dialog.save();
+      });
+
+      this.socket.emit('DIALOG_RECEIVED', {lastMessage});
+    });
+  }
+
+  deleteMessage = (req, res) => {
+    const { messages, dialogId } = req.body;
+
+    MessageModal.deleteMany({_id: {$in: messages}}, err => {
+      if (err) {
+        return res.status(500).json({
+          message: err,
+        });
+      }
+
+      this.updateLastMessage(dialogId);
+      this.updateMessages(dialogId);
     });
   }
 }
