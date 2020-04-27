fd628a086dea4ef3074c16e86e7ab635c9c07c2c
robert <runshacking@gmail.com>
2020-04-27 10:37:42 +0300

    bug fix edit message

--- client/src/sagas/messageEdit.js
+++ client/src/sagas/messageEdit.js
@@ -5,11 +5,13 @@ import { APIMsg } from 'utils/api/msg';
 
 function* fetchEditedMessage() {
   const { editedMessage, id } = yield select(state => state.replyMessage);
+  const { dialogId } = yield select(state => state.dialog);
 
   try {
     yield call(new APIMsg().editedMessage, {
       message: editedMessage, 
-      id
+      id,
+      dialogId
     });
   } catch(err) {
     console.log(err.message); // implement error handling
